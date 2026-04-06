6abe4a8a3e267d2a89e134b4512dc449f65291c5
gab <noonexdddddd@gmail.com>
2026-04-06 07:38:26 -0500

    build files

--- src/main/java/org/hiram/Main.java
+++ src/main/java/org/hiram/Main.java
@@ -10,7 +10,6 @@ import java.time.LocalDate;
 public class Main {
 
     public static void main(String[] args) {
-        // Main Library object
         Library library = new Library();
 
         // Books
